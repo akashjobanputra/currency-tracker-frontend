0c02fcb5caf2e6c16aaeaba4d05edff8921597c6
akashjobanputra <akash.jobanputra@hotmail.com>
2022-02-25 02:33:53 +0530

    Load countries in watch list, and map new countries

--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
-import {
-  Card,
-  Container,
-  Header,
-} from "semantic-ui-react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { Card, Container, Header } from "semantic-ui-react";
 import CountryList from "../../components/CountryList";
 import CountrySearch, { Country } from "../../components/Search";
 
@@ -13,15 +10,83 @@ function roundToTwo(num: number) {
 
 function Dashboard() {
   const [watchList, setWatchList] = useState<Country[]>([]);
+  // const [loading, setLoading] = useState(false);
   const [countries, setCountries] = useState<Country[]>([]);
   const [amountToConvert, setAmountToConvert] = useState(1);
+
+  const MAP_COUNTRY_TO_USER_MUTATION = gql`
+    mutation mapCountryToUser($countryCommonName: String!) {
+      mapCountryToUser(countryCommonName: $countryCommonName) {
+        username
+        id
+        watchList {
+          name {
+            common
+            official
+          }
+          currencies {
+            abbr
+            name
+            exchangeRate
+            symbol
+          }
+        }
+      }
+    }
+  `;
+
+  const QUERY_USER_WATCH_LIST = gql`
+    query {
+      userWatchList {
+        id
+        username
+        watchList {
+          currencies {
+            abbr
+            exchangeRate
+            name
+            symbol
+          }
+          name {
+            common
+            official
+          }
+          population
+        }
+      }
+    }
+  `;
+
+  const [mapCountryToUserMutation, { loading, data, error }] = useMutation(MAP_COUNTRY_TO_USER_MUTATION);
+
+  const { data: dataWatchList } = useQuery(
+    QUERY_USER_WATCH_LIST
+  );
 
   function addToWatchList(country: Country) {
     if (watchList.find((c) => c.name.common === country.name.common)) {
       return;
     }
+    console.log({ country, watchList });
     setWatchList([...watchList, country]);
+    mapCountryToUserMutation({ variables: { countryCommonName: country.name.common } });
   }
+
+  useEffect(() => {
+    if (error) { console.error(error); return; }
+    if (!loading && data) {
+      if (data.mapCountryToUser) {
+        console.log({ data });
+        setWatchList(data.mapCountryToUser.watchList);
+      }
+    }
+  }, [loading, data, error]);
+
+  useEffect(() => {
+    if (dataWatchList) {
+      setWatchList(dataWatchList.userWatchList.watchList);
+    }
+  }, [dataWatchList]);
 
   const COLUMNS = 4;
   const cards = watchList.map((country, idx) => (
@@ -45,10 +110,13 @@ function Dashboard() {
           </ul>
         </Card.Description>
         <Card.Description>
-          <h4>Conversion: {country.currencies[0].symbol}{" "}
-          {roundToTwo(
-            amountToConvert * country.currencies[0]?.exchangeRate || 1
-          )}{` (${country.currencies[0].abbr})`}</h4>
+          <h4>
+            Conversion: {country.currencies[0].symbol}{" "}
+            {roundToTwo(
+              amountToConvert * country.currencies[0]?.exchangeRate || 1
+            )}
+            {` (${country.currencies[0].abbr})`}
+          </h4>
         </Card.Description>
       </Card.Content>
     </Card>
@@ -83,7 +151,9 @@ function Dashboard() {
         )}
       </Header>
       <Container>
-        <Card.Group itemsPerRow={COLUMNS}>{cards}</Card.Group>
+        {!!cards.length
+          ? <Card.Group itemsPerRow={COLUMNS}>{cards}</Card.Group>
+          : <div>No countries found in watch list.</div>}
       </Container>
     </div>
   );
